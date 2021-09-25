d2efefa5065d443f7d526be00a088e41c4616479
proahmedmansour <ahmedmanssur@outlook.com>
2021-09-25 17:07:54 +0200

    Add project files.

---
+++ Pro.RateLimiting/Extensions/ConfigureRateLimitingOptions.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreRateLimit;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+
+namespace Pro.RateLimiting.Extensions
+{
+    public static class StartupConfigurations
+    {
+        public static void ConfigureRateLimitingOptions(this IServiceCollection services)
+        {
+            var rateLimitRules = new List<RateLimitRule>{
+                new RateLimitRule
+                {
+                    Endpoint = "*",
+                    Limit = 3,
+                    Period = "3m"
+                }};
+
+            services.Configure<IpRateLimitOptions>(opt => opt.GeneralRules = rateLimitRules);
+
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+        }
+    }
+}
